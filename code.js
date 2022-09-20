2792848f28d1f42927dbff955a40852de106e72d
Jacob Van Dyke <jacobvandyke194@gmail.com>
2022-09-19 21:28:04 -0600

    almost there

--- code.js
+++ code.js
@@ -1,58 +1,71 @@
 var genBtn = document.getElementById("genBtn").addEventListener("click", genInit);
-var passwordLength = "";
-
-
-function genInit(event){
-    event.preventDefault();
-   
-
+var passwordLength = [];
+var finalPassword = "";
 var lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numberChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var specialChar = ['!', "@", '#', '$', '%', '^', '&', '*', '+'];
-var finalPassword = [];
 
+function genInit(event){
+    event.preventDefault();
+  
 
-    var charLength = prompt("how many characters for you password?")
-        finalPassword = lowerChar.concat(finalPassword);
-    if(charLength <= 15){
-        console.log("correct length")
+    var lengthQuestion = prompt("How many characters between 1 and 15 would you like to include in your password?")
 
-        passwordLength = passwordLength.concat(charLength);
-        console.log(passwordLength);
+    if (lengthQuestion <= 15){
+        var lowerQuestion = confirm("Would you like to include lower case characters?");
+
+        passwordLength = lengthQuestion.concat(passwordLength);
     } else {
-        window.alert("please select a range from 1-15")
-    } 
-    
-    if (charLength <= 15){
-        var upperQuestion = window.confirm("Would you like upper case letters included?")
-    } 
-    
-    if (upperQuestion){
-         finalPassword = upperChar.concat(finalPassword);
-
-         var numberQuestion = window.confirm("would you like to include numbers?")
-    } else if (upperQuestion == false){
-        numberQuestion = window.confirm("would you like to include numbers?")
+        alert("Please select the correct number of characters within the limit");
     }
 
-    if (numberQuestion){
-        finalPassword = numberChar.concat(finalPassword);
+    if (lowerQuestion){
+        finalPassword = lowerChar.concat(finalPassword);
 
-        var specialQuestion = window.confirm("Would you like to add special characters?")
-    } else if (numberQuestion == false){
-        specialQuestion = window.confirm("Would you like to add special characters?")
+        var upperQuestion = confirm("Would you like to include upper case characters?");
+        } else {
+        var upperQuestion = confirm("Would you like to include upper case characters?");
+        }
+    
+    if (upperQuestion){
+        finalPassword = upperChar.concat(finalPassword);
+
+        var specialQuestion = confirm("Would you like to include special characters?");
+    } else {
+        var specialQuestion = confirm("Would you like to include special characters?");
     }
 
     if (specialQuestion){
         finalPassword = specialChar.concat(finalPassword);
 
-        //displayPassword();
-    } else {
-        //displayPassword();
+        var numberQuestion = confirm("Would you like to include numbers?");
+    } else{
+        var numberQuestion = confirm("Would you like to include numbers?");
     }
 
-    console.log(finalPassword)
-    console.log(typeof(charLength))
+    if (numberQuestion){
+        finalPassword = numberChar.concat(finalPassword);
+
+        randomizePassword();
+    } else{
+        randomizePassword();
+    }
+
+
+
+    console.log ("the password length is " + passwordLength);
+    console.log(typeof Number(passwordLength));
+    console.log(finalPassword);
 }
 
+function randomizePassword(){
+    var password = ""
+    for (i = 0; i < Number(passwordLength); i++){
+
+        var randomizedPassword = finalPassword[Math.floor(Math.random()* finalPassword.length)]
+        password = randomizedPassword.concat(password);
+    }
+    return password;
+    
+}
