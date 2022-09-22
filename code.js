e6111c077b28decd6a74351acb469d420f8085bb
Jacob Van Dyke <jacobvandyke194@gmail.com>
2022-09-21 22:42:28 -0600

    finished finally!

--- code.js
+++ code.js
@@ -1,71 +1,72 @@
 var genBtn = document.getElementById("genBtn").addEventListener("click", genInit);
-var passwordLength = [];
+var passwordLength;
 var finalPassword = "";
 var lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numberChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var specialChar = ['!', "@", '#', '$', '%', '^', '&', '*', '+'];
+var options = [];
 
-function genInit(event){
+function genInit(event) {
     event.preventDefault();
-  
+    options = [];
 
-    var lengthQuestion = prompt("How many characters between 1 and 15 would you like to include in your password?")
+    passwordLength = Number(prompt("How many characters between 1 and 15 would you like to include in your password?"))
 
-    if (lengthQuestion <= 15){
-        var lowerQuestion = confirm("Would you like to include lower case characters?");
-
-        passwordLength = lengthQuestion.concat(passwordLength);
-    } else {
+    if (passwordLength > 15) {
         alert("Please select the correct number of characters within the limit");
+        return
     }
 
-    if (lowerQuestion){
-        finalPassword = lowerChar.concat(finalPassword);
 
-        var upperQuestion = confirm("Would you like to include upper case characters?");
-        } else {
-        var upperQuestion = confirm("Would you like to include upper case characters?");
-        }
+    var lowerQuestion = confirm("Would you like to include lower case characters?");
+    if (lowerQuestion) {
+        options = options.concat(lowerChar);
+        
+    }
+
+    var upperQuestion = confirm("Would you like to include upper case characters?");
+
+    if (upperQuestion) {
+        options = options.concat(upperChar);
+        
+    } 
     
-    if (upperQuestion){
-        finalPassword = upperChar.concat(finalPassword);
+    var specialQuestion = confirm("Would you like to include special characters?");
 
-        var specialQuestion = confirm("Would you like to include special characters?");
-    } else {
-        var specialQuestion = confirm("Would you like to include special characters?");
+    if (specialQuestion) {
+        options = options.concat(specialChar);
+        
+    } 
+    
+    var numberQuestion = confirm("Would you like to include numbers?");
+
+    if (numberQuestion) {
+        options = options.concat(numberChar);
+        
     }
+     randomizePassword();
 
-    if (specialQuestion){
-        finalPassword = specialChar.concat(finalPassword);
-
-        var numberQuestion = confirm("Would you like to include numbers?");
-    } else{
-        var numberQuestion = confirm("Would you like to include numbers?");
-    }
-
-    if (numberQuestion){
-        finalPassword = numberChar.concat(finalPassword);
-
-        randomizePassword();
-    } else{
-        randomizePassword();
-    }
-
-
-
-    console.log ("the password length is " + passwordLength);
-    console.log(typeof Number(passwordLength));
-    console.log(finalPassword);
+     console.log("the password length is " + passwordLength);
+     console.log(typeof options);
+    console.log(options);
 }
 
-function randomizePassword(){
-    var password = ""
-    for (i = 0; i < Number(passwordLength); i++){
 
-        var randomizedPassword = finalPassword[Math.floor(Math.random()* finalPassword.length)]
-        password = randomizedPassword.concat(password);
+
+
+
+
+function randomizePassword() {
+    var password = ""
+    for (var i = 0; i < passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * options.length)
+        password += options[randomNumber]
+        console.log(password)
     }
+    var input = document.getElementById("passwordInput");
+    input.value = password;
     return password;
     
 }
+
